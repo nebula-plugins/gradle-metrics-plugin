9164c94b17ea43ab659aefdb008b7ce0dd526c91
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-01-11 09:29:43 -0800

    Enable elastic tests (#48)
    * enable ESMetricsPluginIntegTest
    
    * Created sanitized tests and simplify ESmetrics
    
    * Fix reading buildId for offline mode
    
    * Fix reading buildId for offline mode

---
+++ src/integTest/groovy/nebula/plugin/metrics/SanitizeMetricsIntegTest.groovy
@@ -0,0 +1,86 @@
+package nebula.plugin.metrics
+
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import com.github.tomakehurst.wiremock.junit.WireMockRule
+import nebula.test.IntegrationSpec
+import org.junit.Rule
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import static com.github.tomakehurst.wiremock.client.WireMock.containing
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo
+import static com.github.tomakehurst.wiremock.client.WireMock.post
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor
+
+class SanitizeMetricsIntegTest extends IntegrationSpec {
+
+    @Rule
+    WireMockRule wireMockRule = new WireMockRule(WireMockConfiguration.wireMockConfig().dynamicPort().dynamicPort())
+
+    def 'properties are sanitized via custom regex'() {
+        setup:
+        def regex = "(?i).*\\\\_(ID)\\\$"
+        buildFile << """
+            ${applyPlugin(MetricsPlugin)}
+
+            metrics {
+                sanitizedPropertiesRegex = "$regex"
+                dispatcherType = 'SPLUNK'
+                splunkInputType = 'FORWARDER'
+                splunkUri = 'http://localhost:${wireMockRule.port()}/myserver'
+                headers['Authorization'] = 'Basic YWRtaW46Y2hhbmdlbWU='
+            }
+        """.stripIndent()
+
+
+        //build is finished
+        stubFor(post('/myserver')
+                .withHeader("Authorization", equalTo("Basic YWRtaW46Y2hhbmdlbWU="))
+                .withRequestBody(containing("buildId"))
+                .withRequestBody(containing("startTime"))
+                .withRequestBody(containing("finishedTime"))
+                .withRequestBody(containing("elapsedTime"))
+                .withRequestBody(containing("elapsedTime"))
+                .withRequestBody(containing("project\":{\"name\":\"${moduleName}\",\"version\":\"unspecified\"}"))
+                .withRequestBody(containing("{\"key\":\"MY_ID\",\"value\":\"SANITIZED\"}"))
+                .willReturn(
+                aResponse().withStatus(200).withHeader('Content-Type', 'application/json')))
+
+        expect:
+        runTasksSuccessfully('-DMY_ID=myvalue1', '-Dsomething=value5', 'projects')
+    }
+
+    def 'properties are sanitized via default regex'() {
+        setup:
+        buildFile << """
+            ${applyPlugin(MetricsPlugin)}
+
+            metrics {
+                dispatcherType = 'SPLUNK'
+                splunkInputType = 'FORWARDER'
+                splunkUri = 'http://localhost:${wireMockRule.port()}/myserver'
+                headers['Authorization'] = 'Basic YWRtaW46Y2hhbmdlbWU='
+            }
+        """.stripIndent()
+
+
+        //build is finished
+        stubFor(post('/myserver')
+                .withHeader("Authorization", equalTo("Basic YWRtaW46Y2hhbmdlbWU="))
+                .withRequestBody(containing("buildId"))
+                .withRequestBody(containing("startTime"))
+                .withRequestBody(containing("finishedTime"))
+                .withRequestBody(containing("elapsedTime"))
+                .withRequestBody(containing("elapsedTime"))
+                .withRequestBody(containing("project\":{\"name\":\"${moduleName}\",\"version\":\"unspecified\"}"))
+                .withRequestBody(containing("{\"key\":\"MY_KEY\",\"value\":\"SANITIZED\"}"))
+                .withRequestBody(containing("{\"key\":\"MY_PASSWORD\",\"value\":\"SANITIZED\"}"))
+                .withRequestBody(containing("{\"key\":\"MY_SECRET\",\"value\":\"SANITIZED\"}"))
+                .withRequestBody(containing("{\"key\":\"MY_TOKEN\",\"value\":\"SANITIZED\"}"))
+                .willReturn(
+                aResponse().withStatus(200).withHeader('Content-Type', 'application/json')))
+
+        expect:
+        runTasksSuccessfully('-DMY_KEY=myvalue1', '-DMY_PASSWORD=myvalue2', '-DMY_SECRET=myvalue3', '-DMY_TOKEN=myvalue4', '-Dsomething=value5', 'projects')
+    }
+
+}
